31d3d1f2ceef16b1e46504700845d7393e1212ce
t-k- <clock126@126.com>
2015-09-07 00:22:50 -0400

    finish draft

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ script_dir="$(cd `dirname $0` && pwd)"
 bin_dir=/usr/local/bin
 uniq_name=my-feed-open
 open_script=${uniq_name}.sh
-open_app=${uniq_name}.desktop
+app_dir=/home/tk/.local/share/applications
 mime_dir=/home/tk/.local/share/mime/packages
 feed_ext=tkfd
 
@@ -13,32 +13,34 @@ touch /root/test || exit
 echo "symbol link global command..."
 ln -sf "$script_dir/$open_script" $bin_dir/$open_script
 
-echo "adding ${open_app}..."
-
-cat << EOF | tee ${open_app}
-[Desktop Entry]
-Encoding=UTF-8
-Type=Application
-Name=${uniq_name}
-Exec=touch /tmp/abc
-Terminal=true
-EOF
-#echo ${open_script} %f > /tmp/abc
-
 echo "adding mime xml..."
 mkdir -p ${mime_dir} 
-cat << EOF | tee ${mime_dir}/application-x-foobar.xml 
+cat << EOF | tee ${mime_dir}/application-x-${uniq_name}.xml 
 <?xml version="1.0" encoding="UTF-8"?>
 <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
-<mime-type type="application/x-foobar">
-<comment>foo file</comment>
-<icon name="application-x-foobar"/>
+<mime-type type="application/x-${uniq_name}">
+<comment></comment>
+<icon name="application-x-${uniq_name}"/>
 <glob-deleteall/>
 <glob pattern="*.${feed_ext}"/>
 </mime-type>
 </mime-info>
 EOF
 
+echo "adding application..."
+cat << EOF | tee ${app_dir}/${uniq_name}.desktop
+[Desktop Entry]
+Encoding=UTF-8
+Type=Application
+Name=tk feed open
+Comment=
+Exec=${open_script} %f
+Terminal=true
+StartupNotify=true
+MimeType=application/x-${uniq_name};
+Categories=GNOME;GTK;Utility;TextEditor;
+EOF
+
 echo "update default app database..."
-update-desktop-database ~/.local/share/applications
-update-mime-database    $mime_dir
+update-desktop-database $app_dir
+update-mime-database $mime_dir
