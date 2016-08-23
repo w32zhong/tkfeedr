e177fd3da283cbc67912c50dd9c6d066a755f445
t-k- <clock126@126.com>
2016-08-23 22:42:54 +0800

    'update'

--- tk-feed-add.sh
+++ tk-feed-add.sh
@@ -4,19 +4,21 @@ cat << USAGE
 Description:
 Add feed link to tkfeedr.
 Examples:
-$0 http://teahour.fm/feed.xml
+$0 http://teahour.fm/feed.xml [-f]
 USAGE
 exit
 fi
 
-[ $# -ne 1 ] && echo 'bad arg.' && exit
+[ $# -eq 0 ] && echo 'bad arg.' && exit
 
 url="$1"
 url_id=`echo "${url}" | grep -Po '(?<=//).+?(?=/)'`
 
+force="$2"
+
 echo "Searching domain key: ${url_id}"
 grep "${url_id}" /home/tk/tksync/proj/tkfeedr/list/*.list
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 -a "$force" != "-f" ]; then
 	echo "exists."
 else
 	echo "not exists, adding..."
